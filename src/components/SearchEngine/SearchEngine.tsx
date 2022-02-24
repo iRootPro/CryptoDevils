061cea3342faecef1b5081628e1735da3472dcd6
Alexandr <sao.bur@yandex.ru>
2022-02-24 13:46:28 +0300

    layouts adaptive navbar fix & search fix

--- src/components/SearchEngine/SearchEngine.tsx
+++ src/components/SearchEngine/SearchEngine.tsx
@@ -1,34 +1,37 @@
-import {FC, useCallback} from 'react';
-import {Select} from 'antd';
-import {BaseOptionType, DefaultOptionType} from 'antd/lib/select';
-import {useSelector} from "react-redux";
-import {useHistory} from 'react-router-dom';
-import {useGetCoinsListQuery} from '../../services/api';
+import { FC, useCallback, useLayoutEffect, useState } from 'react';
+import { Select } from 'antd';
+import { BaseOptionType, DefaultOptionType } from 'antd/lib/select';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { useGetCoinsListQuery } from '../../services/api';
 import ROUTES from '../../constants/routes';
-import {useAppDispatch} from "../../hooks/redux";
-import {setSelectCoinForTrade} from "../../redux/reducers/portfolioSlice";
-import {selectSelectedCoinForTrade} from "../../redux/selectors/portfolioSelectors";
+import { useAppDispatch } from '../../hooks/redux';
+import { setSelectCoinForTrade } from '../../redux/reducers/portfolioSlice';
+import { selectSelectedCoinForTrade } from '../../redux/selectors/portfolioSelectors';
 
-const {Option} = Select;
+import useWindowDimensions from '../../hooks/useWindowDimension';
+
+const { Option } = Select;
 
 type OptionT = DefaultOptionType | BaseOptionType | undefined;
 
 type typeProps = {
     // eslint-disable-next-line react/require-default-props
-    getId?: boolean
-}
+    getId?: boolean;
+};
 
-const SearchEngine: FC<typeProps> = ({getId }) => {
+const SearchEngine: FC<typeProps> = ({ getId }) => {
     const { data } = useGetCoinsListQuery('');
-    const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch();
     const history = useHistory();
     const selectedCoinForTrade = useSelector(selectSelectedCoinForTrade);
+    const { width } = useWindowDimensions();
 
     const handlerOnSelect = useCallback((e: string) => {
-        if(getId) {
-            dispatch(setSelectCoinForTrade(e))
+        if (getId) {
+            dispatch(setSelectCoinForTrade(e));
         } else {
-            history.push(`${ROUTES.coin  }/${  e}`);
+            history.push(`${ROUTES.coin}/${e}`);
         }
     }, []);
 
@@ -48,17 +51,29 @@ const SearchEngine: FC<typeProps> = ({getId }) => {
 
     const options = data?.map((d) => <Option key={d.id}>{d.name}</Option>);
 
+    const [searchWidth, setSearchWidth] = useState<string>('100%');
+
+    useLayoutEffect(() => {
+        if (width > 810) setSearchWidth('400px');
+        else setSearchWidth('100%');
+    }, [width]);
+
     return (
         <Select
             showSearch
             allowClear
-            style={{ width: '300px', borderRadius: 10 }}
-            value={ getId ? selectedCoinForTrade : null }
+            style={{
+                width: '100%',
+                maxWidth: searchWidth,
+                borderRadius: 10,
+            }}
+            value={getId ? selectedCoinForTrade : null}
             onSelect={handlerOnSelect}
-            placeholder='Search to Select'
-            optionFilterProp='children'
+            placeholder="Search to Select"
+            optionFilterProp="children"
             filterOption={filterOption}
-            filterSort={filterSort}>
+            filterSort={filterSort}
+        >
             {options}
         </Select>
     );
